145e7b4b9551f00acec481a3d32ee352c4c12734
FilipB <fbachanek@student.agh.edu.pl>
2025-05-20 20:13:34 +0200

    Dodawanie haseł

--- src/PasswordsManager.py
+++ src/PasswordsManager.py
@@ -30,6 +30,32 @@ class ScrollableFrame(ctk.CTkScrollableFrame):
             password_widget = PasswordWidget(text=widget, Passwords=values, master=self)
             password_widget.grid(row=index, column=0, padx=(5,5), pady=(10,0), sticky='w')
 
+class AddPassword(ctk.CTkFrame):
+    def __init__(self, Passwords, ParentFrame, *args, width = 100, height = 32, **kwargs):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.grid_columnconfigure(1,weight=0)
+        self.grid_columnconfigure(0, weight=1)
+        self.Passwords = Passwords
+        self.ParentFrame = ParentFrame
+
+        self.AddButton = ctk.CTkButton(self, text="+", width=height-3, height=height-3, command=self.AddPasswordCallback, corner_radius=6, fg_color='white', text_color='black', hover_color='green')
+        self.AddButton.grid(row=0, column=1, padx=(0,3), pady=3)
+
+        self.PasswordEntry = ctk.CTkEntry(self, text_color='white', height=height-3, fg_color='black', corner_radius=6, placeholder_text='Wpisz hasło')
+        self.PasswordEntry.grid(row=0, column=0, padx=(3,0), pady=3, sticky='ew')
+        self.PasswordEntry.bind('<Return>', command=self.AddPasswordCallback)
+
+    def AddPasswordCallback(self, event=None):
+        text = self.PasswordEntry.get()
+        self.PasswordEntry.delete(0, 'end')
+        if (password := text.strip()) == "" or password in self.Passwords:
+            return None
+        
+        self.Passwords.append(password)
+        NewPasswordWidget = PasswordWidget(password, self.Passwords, master=self.ParentFrame)
+        NewPasswordWidget.grid(row=len(self.Passwords) - 1, column=0, padx=(5,5), pady=(10,0), sticky='w')
+        
 class PasswordManagerClass(ctk.CTkToplevel):
         def __init__(self, Passwords):
             super().__init__()
@@ -47,3 +73,12 @@ class PasswordManagerClass(ctk.CTkToplevel):
 
             self.EasyPasswordsFrame = ScrollableFrame(title='Hasła Łatwe', values=Passwords['Easy'],master=self, fg_color='#24c707')
             self.EasyPasswordsFrame.grid(row=0, column=2, padx=(10,10), pady=(10,10), sticky='nsew')
+
+            self.HardPasswordAddButton = AddPassword(Passwords['Hard'], self.HardPasswordsFrame, master=self)
+            self.HardPasswordAddButton.grid(row=1, column=0, sticky='ew', padx=(5,5), pady=(10,0))
+            
+            self.MediumPasswordAddButton = AddPassword(Passwords['Medium'], self.MediumPasswordsFrame, master=self)
+            self.MediumPasswordAddButton.grid(row=1, column=1, sticky='ew', padx=(5,5), pady=(10,0))
+
+            self.EasyPasswordAddButton = AddPassword(Passwords['Easy'], self.EasyPasswordsFrame, master=self)
+            self.EasyPasswordAddButton.grid(row=1, column=2, sticky='ew', padx=(5,5), pady=(10,0))
