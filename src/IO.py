4e9567d541b067cb296a53103a4270bdeefc1916
FilipB <fbachanek@student.agh.edu.pl>
2025-06-07 21:11:49 +0200

    Refactoring z użyciem biblioteki os

--- src/IO.py
+++ src/IO.py
@@ -1,29 +1,34 @@
+import os
+
 def load_passwords():
     Passwords = {'Hard':[], 'Medium':[], 'Easy':[]}
-    
-    with open(r"passwords\hard.txt", 'rt', encoding='utf-8') as file:
+    path = os.path.join(os.path.abspath(__file__), "..", "..", 'passwords')
+
+    with open(os.path.join(path, 'hard.txt'), 'rt', encoding='utf-8') as file:
         for line in file:
             Passwords['Hard'].append(line.strip())
 
-    with open(r"passwords\easy.txt", 'rt', encoding='utf-8') as file:
+    with open(os.path.join(path, "medium.txt"), 'rt', encoding='utf-8') as file:
         for line in file:
             Passwords['Medium'].append(line.strip())
 
-    with open(r"passwords\medium.txt", 'rt', encoding='utf-8') as file:
+    with open(os.path.join(path, "easy.txt"), 'rt', encoding='utf-8') as file:
         for line in file:
             Passwords['Easy'].append(line.strip())
 
     return Passwords
 
 def save_passwords(Passwords):
-    with open(r"passwords\hard.txt", 'w', encoding='utf-8') as file:
+    path = os.path.join(os.path.abspath(__file__), "..", "..", 'passwords')
+
+    with open(os.path.join(path, 'hard.txt'), 'w', encoding='utf-8') as file:
         for word in Passwords['Hard']:
             file.write(word + "\n")
 
-    with open(r"passwords\medium.txt", 'w', encoding='utf-8') as file:
+    with open(os.path.join(path, "medium.txt"), 'w', encoding='utf-8') as file:
         for word in Passwords['Medium']:
             file.write(word + '\n')
 
-    with open(r"passwords\easy.txt", 'w', encoding='utf-8') as file:
+    with open(os.path.join(path, "easy.txt"), 'w', encoding='utf-8') as file:
         for word in Passwords['Easy']:
             file.write(word + '\n')
